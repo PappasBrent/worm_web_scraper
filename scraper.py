14269aadd0df1eb4cd6aaa80fff7fc39ed24c98d
Brent Pappas <pappasbrent@gmail.com>
2019-02-17 21:13:21 -0500

    Manually open pages

--- scraper.py
+++ scraper.py
@@ -13,33 +13,34 @@ def go_to_ch_dir(chapter_dir_name: str):
 def scrape_worm(cur_link: str, prev_fn=None, ch_no=1):
     prev_fn = None
     while cur_link is not None:
-        with requests.get(cur_link) as page:
-            soup = BeautifulSoup(page.content, "html.parser")
-            ch_title = soup.find("h1", class_="entry-title").get_text()
-            cur_fn = f"{ch_no}-{ch_title}.html"
-            print(cur_fn)
+        page = requests.get(cur_link)
+        # with requests.get(cur_link) as page:
+        soup = BeautifulSoup(page.content, "html.parser")
+        ch_title = soup.find("h1", class_="entry-title").get_text()
+        cur_fn = f"{ch_no}-{ch_title}.html"
+        print(cur_fn)
 
-            links = soup.find_all("a")
-            next_link = None
-            if links:
-                for link in links:
-                    if link.get_text().strip().lower() == "next chapter":
-                        next_link = link.get("href")
+        links = soup.find_all("a")
+        next_link = None
+        if links:
+            for link in links:
+                if link.get_text().strip().lower() == "next chapter":
+                    next_link = link.get("href")
 
-            next_fn = None
-            if next_link:
-                with requests.get(next_link) as next_page:
-                    next_soup = BeautifulSoup(next_page.content, 'html.parser')
-                    next_title = next_soup.find(
-                        "h1", class_="entry-title").get_text()
-                    next_fn = f"{ch_no+1}-{next_title}.html" if next_title else None
+        next_fn = None
+        if next_link:
+            with requests.get(next_link) as next_page:
+                next_soup = BeautifulSoup(next_page.content, 'html.parser')
+                next_title = next_soup.find(
+                    "h1", class_="entry-title").get_text()
+                next_fn = f"{ch_no+1}-{next_title}.html" if next_title else None
 
-            ch_content = soup.find("div", class_="entry-content")
-            ps = ch_content.find_all("p")
-            del ps[0], ps[-1]
+        ch_content = soup.find("div", class_="entry-content")
+        ps = ch_content.find_all("p")
+        del ps[0], ps[-1]
 
-            with open(cur_fn, "w") as ofp:
-                ofp.write(
+        with open(cur_fn, "w") as ofp:
+            ofp.write(
                     f"""
 <!DOCTYPE html>
 <html>
@@ -65,9 +66,10 @@ def scrape_worm(cur_link: str, prev_fn=None, ch_no=1):
 </html>
                 """
                 )
-            prev_fn = cur_fn
-            cur_link = next_link
-            ch_no += 1
+        page.close()
+        prev_fn = cur_fn
+        cur_link = next_link
+        ch_no += 1
 
 
 def main():
